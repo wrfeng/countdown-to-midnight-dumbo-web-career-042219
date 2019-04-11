82a481df5aaf13d8a4bb1c4a76b0f75d1bd0bdbd
wei feng <theweifeng@gmail.com>
2019-04-11 19:22:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  
+  until int == 0 do
+    puts"#{int} SECOND(S)!"
+    int -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(int)
+  
+  until int == 0 do
+    sleep(1)
+    puts"#{int} SECOND(S)!"
+    int -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
 end

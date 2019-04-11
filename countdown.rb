76658cc3d100f68a68174ffd4f854912ae9d7971
wei feng <theweifeng@gmail.com>
2019-04-11 19:22:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,17 @@
 def countdown(int)
   
   until int == 0 do
+    puts"#{int} SECOND(S)!"
+    int -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown(int)
+  
+  until int == 0 do
     sleep(1)
     puts"#{int} SECOND(S)!"
     int -= 1
